2244104c38a22482092989013900b994ae450d1c
Tommaso Torti <torti.tommaso@gmail.com>
2022-04-19 21:11:01 +0200

    straight_flush

--- lib/poker_hand.rb
+++ lib/poker_hand.rb
@@ -1,5 +1,31 @@
+require 'card'
+
 class PokerHand
-    def hello
-        "Hello, world!"
+    
+    VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q','K', 'A']
+
+    def straight_flush?(hand)
+        cards = hand.split.map { |c| Card.new(c) }
+        return same_suit_for_all_cards?(cards) && consecutive_values?(cards)
+    end
+
+    def same_suit_for_all_cards?(cards)
+        suits = cards.map { |card| card.suit }
+        return all_equal?(suits)
+    end
+
+    def consecutive_values?(cards)
+        values = cards.map { |card| card.value }
+        first_value = values[0]
+        index = PokerHand::VALUES.index(first_value)
+        comparison = values.collect do |v|
+            index += 1 
+            PokerHand::VALUES[index] == v 
+        end
+        return comparison.uniq.size == 1
+    end
+
+    def all_equal?(arr)
+        arr.uniq.size <= 1
     end
 end
--- spec/poker_hands_spec.rb
+++ spec/poker_hands_spec.rb
@@ -1,9 +1,10 @@
 require 'poker_hand'
-RSpec.describe PokerHand, '#hello' do
-  context 'for a new project' do
-    it 'greets the world' do
-      pokerHand = PokerHand.new
-      expect(pokerHand.hello).to eq 'Hello, world!'
+RSpec.describe PokerHand  do
+  
+    it 'straight_flush' do
+        pokerHand = PokerHand.new
+        expect(pokerHand.straight_flush?("8C 9C TC JC QC")).to be true
+        expect(pokerHand.straight_flush?("AC 2C 3C 4C 5C")).to be true
+        expect(pokerHand.straight_flush?("7C 9C TC JC QC")).to be false
     end
-  end
-end
+end  
